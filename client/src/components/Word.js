2cade452a458bf18b0b1845031495a5c108e322f
Napster <napster1202@gmail.com>
2020-02-04 16:40:14 +0200

    add new definition modal

--- client/src/components/Word.js
+++ client/src/components/Word.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import AddNewDefinition from "./AddNewDefinition";
 
 export default function Word() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className="pt-3">
       <div className="card p-3">
         <div className="card-body row">
           <h4 className="card-title ml-3">Thi is the definition</h4>
-          {/* <a className="btn btn-outline-success text-right ml-auto">
+          {/* TO DO 
+            Need to be inplemented when user authenticated
+          */}
+          <a
+            onClick={toggle}
+            className="btn btn-outline-success text-right ml-auto"
+          >
             New definition
-          </a> */}
-          <i className="fas fa-edit text-right ml-auto mr-3"></i>
-          <i className="fas fa-trash-alt   mr-3"></i>
+          </a>
+          {/* <i className="fas fa-edit text-right ml-auto mr-3"></i>
+          <i className="fas fa-trash-alt   mr-3"></i> */}
         </div>
+        <AddNewDefinition modal={modal} />
         <div className="definition ml-5 mb-3">
           <div className="card-body">
             <div className="card-text">
