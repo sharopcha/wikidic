fcd40e6275d26fdcf48fe3d2955163a0fc691867
Napster <napster1202@gmail.com>
2020-02-11 05:51:10 +0200

    user auth bug fixed

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 import WordList from "../WordList";
 import Word from "../Word";
 
 export default function Home() {
+  const authContext = useContext(AuthContext);
+  const { user, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (user && isAuthenticated) {
+      <Redirect to="/admin" />;
+    }
+  });
+
   return (
     <div className="row">
-      <div className="col-5 ">
-        <WordList />
-      </div>
-      <div className="col-7 ">
-        <Word />
-      </div>
+      {user && isAuthenticated ? (
+        <Redirect to="/admin" />
+      ) : (
+        <div>
+          <div className="col-5">
+            <WordList />
+          </div>
+          <div className="col-7 ">
+            <Word />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
